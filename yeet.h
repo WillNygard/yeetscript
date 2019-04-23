1867b32e0db84fac6853a78e1c8211ef61cc9749
WillNygard <36652231+WillNygard@users.noreply.github.com>
2019-04-23 15:39:24 -0700

    Add files via upload

---
+++ yeet.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#define yeet int
+#define Yeet main
+#define yEet (
+#define yeEt )
+#define yeeT {
+#define YEet }
+#define yEEt std
+#define yeET ::
+#define YEEt cout
+#define yEET <<
+#define YEET "Yeet!"
+#define YeEt ;
+#define yeett void
---
+++ yeetmain.cpp
@@ -0,0 +1,11 @@
+#include "yeet.h"
+#include "yeetfunc.h"
+
+
+//Yeetspeak
+yeett yeeeet yEet yeEt YeEt
+
+yeet Yeet yEet yeEt yeeT
+    yEEt yeET YEEt yEET YEET YeEt
+    yeET yeeeet yEet yeEt YeEt
+YEet
